462aa9c601ff35319991e999049cac85a4a290aa
bsw1206 <toysw1206@gmail.com>
2026-05-14 09:33:51 +0900

    ch10_3 수정

--- homework/ch10_3/dataframe_cache.py
+++ homework/ch10_3/dataframe_cache.py
@@ -7,40 +7,45 @@ spark = SparkSession \
         .appName('dataframe_cache') \
         .getOrCreate()
 
+print(f'spark application start')
 company_emp_path = 'hdfs:///home/spark/sample/linkedin_jobs/companies/employee_counts.csv'
 company_emp_schema = 'company_id LONG,employee_count LONG,follower_count LONG,time_recorded TIMESTAMP'
 company_ind_path = 'hdfs:///home/spark/sample/linkedin_jobs/companies/company_industries.csv'
 company_ind_schema = 'company_id LONG, industry STRING'
 
-company_emp_df = spark.read.option('header','true').option('multiline','true').schema(company_emp_schema).csv(company_emp_path)
+# employee_counts Load
+company_emp_df = spark.read \
+                 .option('header','true') \
+                 .option('multiLine','true') \
+                 .schema(company_emp_schema) \
+                 .csv(company_emp_path)
 company_emp_df.persist()
 emp_cnt = company_emp_df.count()
-
-
 print(f'company_emp_df count: {emp_cnt}')
 
+# employee_counts 중복 제거
 company_emp_dedup_df = company_emp_df.dropDuplicates(['company_id'])
 emp_dedup_cnt = company_emp_dedup_df.count()
-
 print(f'company_emp_dedup_df count: {emp_dedup_cnt}')
 
-company_ind_df = spark.read.option('header','true').option('multiline','true').schema(company_ind_schema).csv(company_ind_path)
-company_ind_df.persist()
-ind_cnt = company_ind_df.count()
+# company_industries Load
+company_idu_df = spark.read \
+                 .option('header','true') \
+                 .option('multiLine','true') \
+                 .schema(company_ind_schema) \
+                 .csv(company_ind_path)
+company_idu_df.persist()
+idu_cnt = company_idu_df.count()
+print(f'company_idu_df count: {idu_cnt}')
 
-
-print(f'company_ind_df count: {ind_cnt}')
-
-company_it_df = company_ind_df.filter(col('industry') == 'IT Services and IT Consulting')
+company_it_df = company_idu_df.filter(col('industry') == 'IT Services and IT Consulting')
 
 company_emp_cnt_df = company_emp_dedup_df.join(
     other=company_it_df,
     on='company_id',
     how='inner'
-).filter(col('employee_count') >= 1000) \
-    .select('company_id', 'employee_count') \
+).select('company_id', 'employee_count') \
     .sort('employee_count',ascending=False)
-
 
 company_emp_cnt_df.show()
 time.sleep(300)
